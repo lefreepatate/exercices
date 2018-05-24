dcf53398deb9a61e37e83a8ad6495baabb1a7149
cgmuskat <c_garcia_muskat@hotmail.com>
2018-05-24 16:58:49 +0200

    I tried to restore some issues, but have new ones ! We'll talk about it, see you after ;)

--- vehicule.playground/Contents.swift
+++ vehicule.playground/Contents.swift
@@ -1,28 +1,26 @@
 class Vehicle {
-   var registrationNumber: String {
-        get {
-            return self.registrationNumber
-        }
-    }
-     var year: Int {
-        get {
-            return self.year
-        }
-    }
-     var price: Int {
-        get {
-            return self.price
-        }
-    }
-    func startVehicle() {
+ var registrationNumber: String
+ var year: Int
+ var price: Int
+init(registrationNumber: String) {
+self.registrationNumber = registrationNumber
+}
+init(year: Int){
+self.year = year
+}
+init(price: Int){
+self.price = price
+}
+ func startVehicle() {
     }
     func accelerate(){
     }
-    public var description: String {
+    var description: String {
      return "This vehicle has the \(registrationNumber) registration number, created in \(year) and costs \(price)$."
 }
 }
 class Car: Vehicle{
+    
     override func startVehicle(){
         print("Car is starting")
     }
@@ -38,4 +36,8 @@ class Truck: Vehicle{
         print("The truck is accelerating")
     }
 }
+
+
+var myCar = Car(registrationNumber: "765QFR78", year: 2008, price: 5000)
+myCar.description
 
