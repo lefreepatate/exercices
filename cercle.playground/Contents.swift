ccf9ca9563d59eefbb4c74b89ffbbbe6463be7e3
cgmuskat <c_garcia_muskat@hotmail.com>
2018-05-29 15:50:17 +0200

    Var point edited and center_x/center_y added. It's seems to works \o\ |o| /o/ git log

--- cercle.playground/Contents.swift
+++ cercle.playground/Contents.swift
@@ -1,9 +1,14 @@
-class Point: CustomStringConvertible {
+
+class Circle: CustomStringConvertible {
     var x:Double
     var y:Double
-    init(x: Double, y: Double){
+    var center_x:Double
+    var center_y:Double
+    init(x: Double, y: Double,center_x:Double, center_y:Double){
         self.x = x
         self.y = y
+        self.center_x = center_x
+        self.center_y = center_y
     }
     var abscissa:String  {
         return "Donner l'abscisse du centre: \(x)"
@@ -12,28 +17,19 @@ class Point: CustomStringConvertible {
         return "Donner l'ordonné du centre: \(y)"
     }
     var description:String{
-        return "Cercle(\(x), \(y), \(x + y))"
+        return "\nCercle(\(x), \(y), \(x + y))"
     }
     var radius:String {
         return "Donner le rayon : \(x + y)"
     }
-    var point:String{
-        if x <= (x + y) && y <= (x + y) {
-            return "\n\nDonner un point\n\nX :\(x)\n\nY : \(y)\n\nPOINT(\(x),\(y))\n\nLe point appartient au cercle"
-        }else {
-            return "Le point n'appartient pas au cercle"
-        }
-    }
-    
-}
-class Circle: Point{
+
     var perimeter: Double {
         return 2 * Double.pi * (x + y)
     }
     
     var getPerimeter:String {
         get {
-            return "Le perimètre est :\(perimeter)"
+            return "\nLe perimètre est :\(perimeter)"
         }
     }
     var squared:Double {
@@ -41,16 +37,24 @@ class Circle: Point{
     }
     var areaCircle : String{
         get {
-            return "La surface est :\(squared * Double.pi)"
+            return "La surface est :\(squared * Double.pi)\n"
+        }
+    }
+    var point:String{
+        if (x - center_x) * (x - center_x) + (y - center_y) * (y - center_y) <= (x + y) * (x + y) {
+            return "\nDonner un point :\n\nX :\(x)\nY : \(y)\nPOINT(\(x),\(y))\n\nLe point appartient au cercle"
+        }else {
+            return "\nDonner un point :\n\nX :\(x)\nY : \(y)\nPOINT(\(x),\(y))\n\nLe point n'appartient pas au cercle"
         }
     }
 }
-var unCercle = Circle(x:1,y:2)
+var unCercle = Circle(x:1,y:2,center_x:1, center_y:2)
 print(unCercle.abscissa)
 print(unCercle.ordered)
 print(unCercle.radius)
-print(unCercle.description)
+print(unCercle)
 print(unCercle.getPerimeter)
 print(unCercle.areaCircle)
-var unAutreCercle = Circle(x:2,y:3)
+print(unCercle.point)
+var unAutreCercle = Circle(x:2,y:3,center_x:6, center_y:7)
 print(unAutreCercle.point)
